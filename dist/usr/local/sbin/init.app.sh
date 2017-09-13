a3fc2e59316a8ef6ada79d9daff3a655639d8f32
Alexander Günsche <mail@lxg.de>
2017-09-13 21:47:09 +0200

    add mysql config for remote container

--- dist/usr/local/sbin/init.app.sh
+++ dist/usr/local/sbin/init.app.sh
@@ -7,19 +7,28 @@ cd /srv/www
 
 # insert config values from environment
 
-for param in database_driver database_server_version database_host database_port database_name database_user database_password mailer_transport mailer_host mailer_port mailer_user mailer_password mailer_encryption mailer_authmode mailer_testaccount; do
+for param in database_driver database_server_version database_host database_port database_name database_user database_pass mailer_transport mailer_host mailer_port mailer_user mailer_password mailer_encryption mailer_authmode mailer_testaccount; do
     echo "$param: ${!param}" >> /tmp/test.txt
     [ -z "${!param}" ] || perl -i -pe "s|$param\s?:.*$|$param: ${!param}|g" app/config/parameters.yml
 done
 
 # wait until database server is started and DB is initialized
 
+mysql_command="mysql"
+dbhost="$(grep database_name app/config/parameters.yml | perl -pe 's|^.*database_host\s?:\s*||g')"
 dbname="$(grep database_name app/config/parameters.yml | perl -pe 's|^.*database_name\s?:\s*||g')"
+dbuser="$(grep database_name app/config/parameters.yml | perl -pe 's|^.*database_user\s?:\s*||g')"
+dbpass="$(grep database_name app/config/parameters.yml | perl -pe 's|^.*database_pass\s?:\s*||g')"
+
 try_count=0
 ok=0
 
 if [ "$dbname" ]; then
-    until mysql -e "USE $dbname" && ok=1; do
+    [[ -z "$dbhost" || "$dbhost" = "null" ]] || mysql_command="$mysql_command -h$dbhost"
+    [[ -z "$dbuser" || "$dbuser" = "null" ]] || mysql_command="$mysql_command -u$dbuser"
+    [[ -z "$dbpass" || "$dbpass" = "null" ]] || mysql_command="$mysql_command -p$dbpass"
+
+    until $mysql_command -e "USE $dbname" && ok=1; do
         [ $((++try_count)) -gt 30 ] && break
         sleep 1
     done
@@ -29,6 +38,8 @@ if [ $ok -gt 0 ]; then
     app/console doctrine:schema:update --force --complete
     app/console cache:clear
     app/console agit:seeds:update
+
+    chown -R www-data: /srv/www/app/cache /srv/www/app/logs
 else
     exit 1
 fi
